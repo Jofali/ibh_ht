3aa3cacb59d29d9cd5a671201b3e1fe82102c31b
uncle <247686992@qq.com>
2017-08-30 18:51:23 +0800

    打包

--- ibh_ht/src/components/signup.vue
+++ ibh_ht/src/components/signup.vue
@@ -10,13 +10,16 @@
           <el-input v-model="email" placeholder="注册邮箱"></el-input>
         </li>
         <li>
+          <el-input v-model="nickName" placeholder="昵称"></el-input>
+        </li>
+        <li>
           <el-input v-model="password" placeholder="设置密码" type="password"></el-input>
         </li>
         <li>
-          <el-input v-model="password" placeholder="再次输入密码" type="password"></el-input>
+          <el-input v-model="againPassword" placeholder="再次输入密码" type="password"></el-input>
         </li>
         <li>
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="registered()">注册</el-button>
         </li>
       </ul>
     </el-col>
@@ -26,7 +29,26 @@
 
 <script>
 export default {
-  name: 'signup'
+  name: 'signup',
+  data () {
+    return {
+      email: '',
+      password: '',
+      againPassword: '',
+      nickName: '',
+      num: ['写数据库失败', '注册成功', '邮箱已注册', '昵称已注册']
+    }
+  },
+  methods: {
+    registered: function () {
+      const self = this
+      this.$axios.get('http://localhost:1736/api/UserInfo/GetRegister?Email=' + self.email + '&Password=' + self.password + '&Nickname=' + self.nickName).then(function (response) {
+        self.$alert(self.num[response.data], '提示', {
+          confirmButtonText: '确定'
+        })
+      })
+    }
+  }
 }
 </script>
 
