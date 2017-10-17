7735a1d8427de8939fe3420d952acd6a64008f07
uncle <247686992@qq.com>
2017-10-17 16:40:15 +0800

    打包

--- ibh_ht/src/components/signin.vue
+++ ibh_ht/src/components/signin.vue
@@ -38,7 +38,7 @@ export default {
       rules2: {
         email: [
            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-           { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
+           { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
         ],
         password: [
           { validator: checkPassword, trigger: 'blur, change' }
